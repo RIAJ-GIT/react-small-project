d8c3e725dc2d9ee47e673f0e78d024e087ee85bb
RIAJ-GIT <33200121135@tib.edu.in>
2024-01-11 18:58:14 +0530

    background changer app

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(15)
-  
-  const addvalue =() =>{
-setCount(count +1)
- }
-const removevalue=()=>{
-setCount(count -1)
- }
+  const [color , setColor] = useState('black')
 
   return (
-    <>
-      <h1>counter app</h1>
-      <h2>count {count}</h2>
-      <button onClick={addvalue}>add value</button>{" "}
-      <button onClick={removevalue}>remove value</button>{" "}
-      
-    </>
+    
+    <div className='w-full h-screen duration-200 ' style={{backgroundColor: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+        <button
+          onClick={() => setColor('red')}
+          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
+          style={{backgroundColor: 'red'}}
+          >Red</button>
+          <button
+          onClick={() => setColor('green')}
+          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
+          style={{backgroundColor: 'green'}}
+          >Green</button>
+          <button onClick={()=>setColor('grey')}
+          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
+          style={{backgroundColor:'grey'}}>
+            grey
+          </button>
+
+          </div>
+          </div>
+          </div>
   )
 }
 
