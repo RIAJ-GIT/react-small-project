e0636eb3d78f376ed3d3634d9f2ca643a9516728
RIAJ-GIT <33200121135@tib.edu.in>
2024-01-19 00:32:39 +0530

    password generator app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,101 @@
-import { useState } from 'react'
+import { useState ,useCallback,useEffect,useRef} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [color , setColor] = useState('black')
+  const [length, setLength] = useState(7)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState('')
+
+  const passwordRef = useRef(null)
+
+  const generatePassword = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*()_+"
+
+    for(let i = 0; i < length; i++) {
+      const char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+    }
+
+    setPassword(pass)
+
+  }, [length, numberAllowed, charAllowed])
+
+  const copyPasswordToClipboard = () => {
+    window.navigator.clipboard.writeText(password)
+    passwordRef.current.select()
+    
+  }
+
+
+  useEffect(()=>{
+    generatePassword()
+  }, [length, numberAllowed, charAllowed])
 
   return (
-    
-    <div className='w-full h-screen duration-200 ' style={{backgroundColor: color}}>
-      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+      <h1 className='text-white text-center my-3'>Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input 
+        type="text"
+        value={password}
+        className='outline-none w-full py-1 px-3'
+        placeholder='Password'
+        readOnly
+        ref={passwordRef}
+        />
         <button
-          onClick={() => setColor('red')}
-          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
-          style={{backgroundColor: 'red'}}
-          >Red</button>
-          <button
-          onClick={() => setColor('green')}
-          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
-          style={{backgroundColor: 'green'}}
-          >Green</button>
-          <button onClick={()=>setColor('grey')}
-          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
-          style={{backgroundColor:'grey'}}>
-            grey
-          </button>
-
-          </div>
-          </div>
-          </div>
+        onClick={copyPasswordToClipboard}
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >copy</button>
+      </div>
+      <div
+      className='flex text-sm gap-x-2'
+      >
+        <div className='flex items-center gap-x-1'>
+          <input 
+          type="range" 
+          min={6}
+          max={100}
+          value={length}
+          className='cursor-pointer'
+          onChange={(e) => setLength(e.target.value)}
+          name="" 
+          id=""
+           />
+           <label htmlFor="length">Length: {length}</label>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input 
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          onChange={() => {
+            setNumberAllowed((prev) => !prev)
+          }}
+           name=""
+          id="" />
+          <label htmlFor="number">Numbers</label>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input 
+          type="checkbox"
+          defaultChecked={charAllowed}
+          onChange={() => {
+            setCharAllowed((prev) => !prev)
+          }}
+           name=""
+          id="" />
+          <label htmlFor="charInput">Character</label>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
